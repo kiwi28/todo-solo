af563d112ecb0285bb95b3eaa07d18aa72d6006f
Alex Chivulescu <alex.chivulescu@dotts.ro>
2021-02-27 16:41:22 +0200

    dev ed 20:48

---
+++ src/sayHello.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Hello() {
+  const sayHello = () => {
+    console.log("hello");
+  };
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <button onClick={sayHello}>Say Hello</button>
+    </div>
+  );
+}
+
+export default Hello;
